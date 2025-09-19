dfdbe4eec206dd99ceca8f186b36fb1cdd2d5bc7
EduRls <toolstrabajos@gmail.com>
2025-09-18 18:36:38 -0600

    Cambios en el controller y models

--- app/Http/Controllers/CfdiController.php
+++ app/Http/Controllers/CfdiController.php
@@ -8,31 +8,45 @@ use App\Models\Cfdi;
 class CfdiController extends Controller
 {
     public function store(Request $request)
-    {
-        $request->validate([
-            'evento_id' => 'required|exists:eventos_almacen,id',
-            'UUID' => 'required|string',
-            'RFCEmisorCFDI' => 'required|string',
-            'NombreEmisorCFDI' => 'nullable|string',
-            'RFCProveedorReceptor' => 'nullable|string',
-            'MontoTotalOperacion' => 'nullable|numeric',
-            'FechaCFDI' => 'required|date'
-        ]);
+{
+    $request->validate([
+        'evento_id' => 'required|exists:eventos_almacen,id',
 
-        $cfdi = Cfdi::create([
-            'evento_id' => $request->evento_id,
-            'TipoComplemento' => 'CFDI',
-            'Version' => '1.0',
-            'UUID' => $request->UUID,
-            'RFCEmisorCFDI' => $request->RFCEmisorCFDI,
-            'NombreEmisorCFDI' => $request->NombreEmisorCFDI,
-            'RFCProveedorReceptor' => $request->RFCProveedorReceptor,
-            'MontoTotalOperacion' => $request->MontoTotalOperacion,
-            'FechaCFDI' => $request->FechaCFDI,
-        ]);
+        // estas llaves llegan desde el front en PascalCase
+        'UUID'                 => 'required|string',
+        'RFCEmisorCFDI'        => 'required|string',
+        'NombreEmisorCFDI'     => 'nullable|string',
+        'RFCProveedorReceptor' => 'nullable|string',
+        'MontoTotalOperacion'  => 'nullable|numeric',
+        'FechaCFDI'            => 'required|date',
 
-        return response()->json(['success' => true, 'cfdi' => $cfdi]);
-    }
+        // si también te mandan estos, valídalos:
+        'TipoComplemento'            => 'nullable|string',
+        'Version'                    => 'nullable|string',
+        'TipoCFDI'                   => 'nullable|string',
+        'VolumenDocumentadoValor'    => 'nullable|numeric',
+        'VolumenDocumentadoUnidad'   => 'nullable|string',
+    ]);
+
+    // Mapeo PascalCase (request) -> snake_case (DB)
+    $cfdi = Cfdi::create([
+        'evento_id'                   => $request->input('evento_id'),
+        'tipo_complemento'            => $request->input('TipoComplemento', 'CFDI'),
+        'version'                     => $request->input('Version', '1.0'),
+        'uuid'                        => $request->input('UUID'),
+        'rfc_emisor_cfdi'             => $request->input('RFCEmisorCFDI'),
+        'nombre_emisor_cfdi'          => $request->input('NombreEmisorCFDI'),
+        'rfc_proveedor_receptor'      => $request->input('RFCProveedorReceptor'),
+        'monto_total_operacion'       => $request->input('MontoTotalOperacion'),
+        'fecha_cfdi'                  => $request->input('FechaCFDI'),
+        'tipo_cfdi'                   => $request->input('TipoCFDI'),
+        'volumen_documentado_valor'   => $request->input('VolumenDocumentadoValor'),
+        'volumen_documentado_unidad'  => $request->input('VolumenDocumentadoUnidad'),
+    ]);
+
+    return response()->json(['success' => true, 'cfdi' => $cfdi], 201);
+}
+
 
     public function index($evento_id)
     {
